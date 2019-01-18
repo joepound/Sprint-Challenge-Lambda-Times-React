67e5566109a0c8794d7c97f4da07dbe78203425c
joepound <14357797+joepound@users.noreply.github.com>
2019-01-18 11:01:17 -0700

    Card components rendered

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -20,7 +20,8 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
-    const tabs = tabData, cards = cardData;
+    const tabs = tabData,
+      cards = cardData;
     this.setState({
       tabs,
       cards
