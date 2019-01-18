d3e67bd3a8dfae74dac1698309920207d860e17b
joepound <14357797+joepound@users.noreply.github.com>
2019-01-18 10:51:24 -0700

    Tab components rendered

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,30 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Tabs from './Tabs';
-import Cards from './Cards';
+import Tabs from "./Tabs";
+import Cards from "./Cards";
 
 // Importing our tab and card data. No need to change anything here.
-import { tabData, cardData } from '../../data';
+import { tabData, cardData } from "../../data";
 
 export default class Content extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: 'all',
+      selected: "all",
       tabs: [],
       cards: []
     };
   }
 
+  // Life cycle methods
+
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    const tabs = tabData, cards = cardData;
+    this.setState({
+      tabs,
+      cards
+    });
   }
 
-  changeSelected = tab => {
-    // this function should take in the tab and update the state with the new tab.
-  };
+  // Event-based methods
 
-  filterCards = () => {
+  changeSelected(tab) {
+    // this function should take in the tab and update the state with the new tab.
+  }
+
+  filterCards() {
     /* Right now this function only returns the cards on state.
       We're going to make this function more dynamic
       by using it to filter out our cards for when a tab is selcted
@@ -32,12 +41,22 @@ export default class Content extends Component {
       Notice that we're passing this function to our <Cards /> component below.
       This function returns an array of cards, so we can just pass it down as such.
 
-      Your algorithim for the logic here is as follows: 
+      Your algorithm for the logic here is as follows: 
         - if the selected tab is 'all' it should return all 
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
     return this.state.cards;
+  }
+
+  // Event handler methods
+
+  handleClick = e => {
+    switch (e.currentTarget.name) {
+      case "tab-selection":
+        this.changeSelectedTab(e.currentTarget.dataset.tab);
+        break;
+    }
   };
 
   render() {
@@ -45,11 +64,17 @@ export default class Content extends Component {
       <div className="content-container">
         {/* 
           Add 2 props to the Tabs component, 
-          `selectedTab` that includes the currently selected tab
-          and `selectTabHandler` that includes the function to change the selected tab
+          
+          * `selectedTab` that includes the currently selected tab
+          
+          * and `selectTabHandler` that includes the function to change the selected tab -> will do this with an event handler instead
         */}
-        <Tabs tabs={this.state.tabs} />
-        <Cards cards={this.filterCards()} />
+        <Tabs
+          tabs={this.state.tabs}
+          selectedTab={this.state.selected}
+          handleClick={this.handleClick}
+        />
+        <Cards cards={this.state.cards} />
       </div>
     );
   }
--- src/components/Content/Tab.js
+++ src/components/Content/Tab.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+
   return (
     <div
-      className={''}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-      }}
+      className={
+        props.selectedTab === props.tabName || props.selectedTab === "all"
+          ? "tab active-tab"
+          : "tab"
+      }
+      /* Replace this dummy click handler function with your selectTabHandler function from props 
+         you'll need to pass the `tab` in as an argument to this handler. -> everything needed will be in the event */
+      name="tab-selection"
+      data-tab={props.tab}
+      onClick={props.handleClick}
     >
-      {props.tab.toUpperCase()}
+      {props.tabName.toUpperCase()}
     </div>
   );
 };
 
 // Make sure you include PropTypes on your props.
+Tab.propTypes = {
+  tabName: PropTypes.string.isRequired,
+  selectedTab: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired
+};
 
 export default Tab;
