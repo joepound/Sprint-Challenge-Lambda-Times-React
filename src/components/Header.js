7db71e8a97ab04b4327038a20575057e405607f4
joepound <14357797+joepound@users.noreply.github.com>
2019-01-18 11:45:48 -0700

    Header refactored to use Styled Components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,62 @@
-import React from 'react';
+import React from "react";
 
-// Refactor this component to use styled components and not classNames. 
+import styled, { css } from "styled-components";
+
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
 const Header = () => {
-  return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
-  )
-}
+  const LambdaTimesHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row;
+    background-color: #fff;
+    border-bottom: 1px solid lightgrey;
+    width: 100%;
+    height: 120px;
+    margin-top: 44px;
+    padding-bottom: 15px;
 
-export default Header
+    @media (min-width: 1280px) {
+      width: 1280px;
+    }
+  `;
+
+  const HeaderData = styled.span`
+    align-self: flex-end;
+    font-size: 11px;
+    font-weight: bold;
+    letter-spacing: 1px;
+  `;
+
+  const Date = styled(HeaderData)`
+    margin-left: 25px;
+    flex: 1;
+  `;
+
+  const TitleHeading = styled.h1`
+    font-size: 60px;
+    font-family: Didot, serif;
+    font-weight: bold;
+    flex: 8;
+    text-align: center;
+    color: #000;
+  `;
+
+  const Temperature = styled(HeaderData)`
+    text-align: right;
+    margin-right: 25px;
+    flex: 1;
+  `;
+
+  return (
+    <LambdaTimesHeader>
+      <Date>SMARCH 32, 2018</Date>
+      <TitleHeading>Lambda Times</TitleHeading>
+      <Temperature>98°</Temperature>
+    </LambdaTimesHeader>
+  );
+};
+
+export default Header;
