fab3a49e7f3f71d6d15ac7bc772e717b38230fb0
joepound <14357797+joepound@users.noreply.github.com>
2019-01-18 12:39:04 -0700

    Card components refactored to use Styled Components

--- src/components/Content/Card.js
+++ src/components/Content/Card.js
@@ -1,17 +1,61 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import styled from "styled-components";
+
 const Card = props => {
+  const CardBlock = styled.div`
+    border-bottom: 1px solid lightgrey;
+    display: flex;
+    justify-content: space-between;
+    align-items: none;
+    flex-direction: column;
+    background-color: #fff;
+    width: 380px;
+    margin-bottom: 16px;
+    padding: 24px;
+  `;
+
+  const CardHeadline = styled.div`
+    font-size: 25px;
+    font-family: "Didot", serif;
+  `;
+
+  const CardAuthorBlock = styled.div`
+    display: flex;
+    justify-content: none;
+    align-items: center;
+    flex-direction: row;
+    margin-top: 15px;
+  `;
+
+  const CardAuthorImgBlock = styled.div`
+    padding-right: 10px;
+    border-right: 1px solid lightgrey;
+    height: 40px;
+  `;
+
+  const CardAuthorImgElement = styled.img`
+    width: 40px;
+  `;
+
+  const CardAuthorElement = styled.span`
+    padding-left: 10px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+  `;
+
   return (
-    <div className="card">
-      <div className="headline">{props.card.headline}</div>
-      <div className="author">
-        <div className="img-container">
-          <img src={props.card.img} />
-        </div>
-        <span>By {props.card.author}</span>
-      </div>
-    </div>
+    <CardBlock>
+      <CardHeadline>{props.card.headline}</CardHeadline>
+      <CardAuthorBlock>
+        <CardAuthorImgBlock>
+          <CardAuthorImgElement src={props.card.img} />
+        </CardAuthorImgBlock>
+        <CardAuthorElement>By {props.card.author}</CardAuthorElement>
+      </CardAuthorBlock>
+    </CardBlock>
   );
 };
 
